c21b85cc621b4eba5e43e35727da0ed2d250ee21
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2025-08-11 01:41:40 -0400

    Update transportation.py

--- transportation.py
+++ transportation.py
@@ -1,11 +1,32 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 accident=pd.read_csv("/Users/sangeetha/Downloads/FARS2023NationalCSV/accident.csv")
 
 print(accident)
 #There were 37654 accidents in 2023
 
-print(accident.isnull())
+print(accident.isnull().sum())
 
-weather=pd.read_csv("/Users/sangeetha/Downloads/FARS2023NationalCSV/weather.csv")
+state_counts= accident ['STATENAME'].value_counts().sort_values(ascending=False)
+print(state_counts)
+
+plt.figure(figsize=(12,6))
+state_counts.plot(kind='bar')
+plt.xlabel('State')
+plt.ylabel('Number of Accidents')
+plt.title('Number of Accidents by State in the US - 2023')
+plt.tight_layout()
+plt.show()
+
+state_deaths=accident.groupby('STATENAME')['FATALS'].sum().sort_values(ascending=False)
+print(state_deaths)
+
+plt.figure(figsize=(12,6))
+state_deaths.plot(kind='bar', color='red')
+plt.xlabel('State')
+plt.ylabel('Number of Casualities')
+plt.title('Number of Deaths from a Car Accident in 2023')
+plt.tight_layout()
+plt.show()
